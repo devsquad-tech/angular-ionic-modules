ae678c1ba304ce348ecc516b3be28b82240ea22a
Ramon Henrique Ornelas <ramon.ornela@gmail.com>
2017-09-10 20:32:37 -0300

    test(config): add more tests

--- packages/config/test/config.provider.spec.ts
+++ packages/config/test/config.provider.spec.ts
@@ -1,13 +1,45 @@
-import './fix-test';
-import { async, TestBed } from '@angular/core/testing';
-import { Config } from '../src';
+import { bootstrap } from './fix-test';
+import { async, inject, TestBed } from '@angular/core/testing';
+import { Config, ConfigData } from '../src';
+
+function configProvider(data?: any) {
+  TestBed.configureTestingModule({
+    providers: [
+      { provide: ConfigData, useValue: data || {}},
+      Config,
+    ]
+  });
+}
 
 describe('Config Provider', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      providers: [
-        Config,
-      ]
+  beforeAll(() => {
+    bootstrap();
+  });
+  beforeEach(() => {
+    configProvider({
+      'dev': {
+        'key': 'value'
+      }
     });
+  });
+
+  it('Get all', inject([Config],(config: Config) => {
+    const keys = Object.keys(config.getAll());
+    expect(keys.length).toBe(1);
+  }));
+
+  it('Get value', inject([Config],(config: Config) => {
+    expect(config.get('key', 'dev')).toBe('value');
+  }));
+
+  it('Set value', inject([Config],(config: Config) => {
+    const chainConfig = config.set('key2', 'foo', 'dev');
+    expect(chainConfig).toBe(config);
+    expect(config.get('key2', 'dev')).toBe('foo');
+  }));
+
+  it('Set value section not exists', inject([Config],(config: Config) => {
+    config.set('keyProd', 'secret', 'prod');
+    expect(config.get('keyProd', 'prod')).toBe('secret');
   }));
 });
--- packages/config/test/fix-test.ts
+++ packages/config/test/fix-test.ts
@@ -1,4 +1,14 @@
+import 'reflect-metadata';
 import 'zone.js';
 import 'zone.js/dist/proxy';
 import 'zone.js/dist/sync-test';
 import 'zone.js/dist/jasmine-patch';
+import { TestBed } from '@angular/core/testing';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+
+export function bootstrap() {
+  TestBed.initTestEnvironment(
+    BrowserDynamicTestingModule,
+    platformBrowserDynamicTesting()
+  );
+}
