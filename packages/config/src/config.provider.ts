7e9f391c703cfa71a180a8b0aa73e2acf93c5ec9
Ramon Henrique Ornelas <ramon.ornela@gmail.com>
2017-11-05 16:23:35 -0200

    refactor(config): remove extends env

--- packages/config/src/config.provider.ts
+++ packages/config/src/config.provider.ts
@@ -12,14 +12,7 @@ export class Config {
   private sectionsExtends: {[section: string]: string} = {};
 
   constructor(@Inject(ConfigData) private data: Data) {
-    // check if assign extends in keys ex: dev:prod -> dev inherits prod
-    const sections = Object.keys(this.data);
-    for (const section of sections) {
-      if (section.indexOf(':') !== -1) {
-        const [ sectionMain, sectionExtends ] = section.split(':');
-        this.setSectionExtends(sectionMain, sectionExtends);
-      }
-    }
+    // @todo validate section
   }
 
   /**
@@ -27,7 +20,7 @@ export class Config {
    *
    * @return {Data}
    */
-  getAll(): Data {
+  get(): Data {
     return this.data;
   }
 
@@ -37,9 +30,9 @@ export class Config {
    * @param {string} section that can used how enviroment
    * @return {Config} fluent interface
    */
-  set(key: string, value: any, section: string): this {
+  add(key: string, value: any, section: string): this {
     // case section not exists create empty
-    if (!this.searchSection(section)) {
+    if (!this.data[section]) {
       this.data[section] = {};
     }
 
@@ -49,87 +42,17 @@ export class Config {
   }
 
   /**
-   * @param {string} section section main
-   * @param {string} sectionExtends section that the main inherits
-   * @return {Config} fluent interface
-   */
-  setSectionExtends(section: string, sectionExtends: string): this {
-    // can't inherits case not exists in data
-    if (!this.searchSection(sectionExtends)) {
-      const messageException = `Not allow inherits '${sectionExtends}' because not exists in data`;
-      throw new Error(messageException);
-    }
-
-    // case section main not exists create empty
-    if (!this.searchSection(section)) {
-      this.data[section] = {};
-    }
-
-    this.sectionsExtends[section] = sectionExtends;
-
-    return this;
-  }
-
-  /**
-   * Get name inherits of a section
-   *
-   * @param {string} section
-   * @return {string}
-   */
-  getSectionExtends(section: string): string {
-    // case section not inherits throw exception
-    if (!this.sectionsExtends[section]) {
-      throw new Error(`Section '${section}' not exists`);
-    }
-
-    return this.sectionsExtends[section];
-  }
-
-  /**
-   * Get value in section or in inherits
+   * Get value in section
    *
    * @param {string} key
    * @param {string} section
    * @return {any}
    */
-  get(key: string, section: string): any {
-    // if section not exists throw exception
-    let searchSection: any = this.searchSection(section);
-    if (!searchSection) {
+  getByKey(key: string, section: string): any {
+    if (!(section in this.data)) {
       throw new Error(`Section ${section} not exists`);
     }
 
-    // case exists key return to not check extends
-    if (key in this.data[searchSection]) {
-      return this.data[searchSection][key];
-    }
-
-    // get name section extends
-    const sectionExtends = this.getSectionExtends(section);
-    searchSection = this.searchSection(sectionExtends);
-
-    return this.data[searchSection][key] || null;
-  }
-
-  /**
-   * Return section simple ex: 'dev', extends 'dev:prod' or false
-   *
-   * @param {string} section
-   * @return {string|boolean}
-   */
-  private searchSection(section: string) {
-    if (section in this.data) {
-      return section;
-    }
-
-    const sections = Object.keys(this.data);
-
-    for (const indexSection of sections) {
-      if (indexSection.indexOf(`${section}:`) !== -1) {
-        return indexSection;
-      }
-    }
-
-    return false;
+    return this.data[section][key] || null;
   }
 }
--- packages/config/test/config.provider.spec.ts
+++ packages/config/test/config.provider.spec.ts
@@ -16,49 +16,24 @@ describe('Config Provider', () => {
       'dev': {
         'key': 'value'
       },
-      'staging:dev': {
+      'staging': {
         'foo': 'bar'
       }
     });
   });
 
   it('Get all', inject([ Config ], (config: Config) => {
-    const keys = Object.keys(config.getAll());
+    const keys = Object.keys(config.get());
     expect(keys.length).toBe(2);
   }));
 
-  it('Get value', inject([ Config ], (config: Config) => {
-    expect(config.get('key', 'dev')).toBe('value');
+  it('Get by key', inject([ Config ], (config: Config) => {
+    expect(config.getByKey('key', 'dev')).toBe('value');
   }));
 
-  it('Set value', inject([ Config ], (config: Config) => {
-    const chainConfig = config.set('key2', 'foo', 'dev');
+  it('Add value', inject([ Config ], (config: Config) => {
+    const chainConfig = config.add('key2', 'foo', 'dev');
     expect(chainConfig).toBe(config);
-    expect(config.get('key2', 'dev')).toBe('foo');
-  }));
-
-  it('Set value section not exists', inject([ Config ], (config: Config) => {
-    config.set('keyProd', 'secret', 'prod');
-    expect(config.get('keyProd', 'prod')).toBe('secret');
-  }));
-
-  it('Section extends', inject([ Config ], (config: Config) => {
-    config.setSectionExtends('prod', 'dev');
-    expect(config.getSectionExtends('prod')).toBe('dev');
-  }));
-
-  it('Get value extends', inject([ Config ], (config: Config) => {
-    expect(config.get('key', 'staging')).toBe('value');
-    expect(config.get('foo', 'staging')).toBe('bar');
-  }));
-
-  it('Set section inherits not exists', inject([ Config ], (config: Config) => {
-    expect(() => config.setSectionExtends('prod', 'dev2'))
-     .toThrow(new Error('Not allow inherits \'dev2\' because not exists in data'));
-  }));
-
-  it('Get Section inherits not exists', inject([ Config ], (config: Config) => {
-    expect(() => config.getSectionExtends('prod'))
-     .toThrow(new Error('Section \'prod\' not exists'));
+    expect(config.getByKey('key2', 'dev')).toBe('foo');
   }));
 });
